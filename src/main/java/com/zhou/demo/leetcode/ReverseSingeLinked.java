1bd14ccd5b1a068d9cf5164252417133e171e4eb
jackZhou <zhouzhen01@gridsum.com>
2021-02-20 21:18:08 +0800

    反转相关leetcode提交

---
+++ src/main/java/com/zhou/demo/leetcode/ReverseSingeLinked.java
@@ -0,0 +1,143 @@
+package com.zhou.demo.leetcode;
+
+import lombok.Data;
+
+/**
+ * @ClassName reverseSingeLinked
+ * @Author JackZhou
+ * @Date 2021/1/17  19:46
+ **/
+
+// 反转单链表 1—>2->3->4  4->3-2-1
+public class ReverseSingeLinked {
+
+    public static void main(String[] args) {
+        // 构建单链表
+        Node node =  new Node(1);
+        for(int i = 2; i < 6; i++){
+            node.add(i);
+        }
+        System.out.println(node.toString());
+
+        //递归和循环解决
+        //System.out.println(reverRecursion(node));
+        //System.out.println(rever(node));
+
+        // leetcode  92
+        testReverseBetween(node, 2, 5);
+
+    }
+
+    // 分治思想   反转N N-1 反转 1
+    public static Node reverRecursion(Node node){
+        if(node == null || node.getNext() == null){
+            return node;
+        }
+        Node temp = node.getNext();
+        Node rever = reverRecursion(temp);
+        temp.setNext(node);
+        node.setNext(null);
+        return rever;
+    }
+
+    // 循环处理
+    public static Node rever(Node node){
+        if(node == null || node.getNext() == null){
+            return node;
+        }
+        Node current = node;
+        Node next = node.getNext();
+        node.setNext(null); // 设置为null，不然循环调用栈溢出
+        while(next != null){
+            Node temp = next.getNext();
+            next.setNext(current);
+            current = next;
+            next = temp;
+        }
+        return current;
+    }
+
+
+    public static void testReverseBetween(Node head, int left, int right){
+        System.out.println(reverseBetween(head, left, right).toString());
+    }
+
+    // https://leetcode-cn.com/problems/reverse-linked-list-ii/
+    // .92 反转链表 II     反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+    // 说明 1 ≤ m ≤ n ≤ 链表长度。
+    //输入: 1->2->3->4->5->NULL, m = 2, n = 4
+    // 输出: 1->4->3->2->5->NULL
+    public static Node reverseBetween(Node head, int left, int right) {
+        if(left == right){
+            return  head;
+        }
+        // 找到开始的节点和开始的前一个节点
+        //如果left节点为1、开始节点的前一个节点为null
+        Node beginNodeBefore =  null;
+        Node beginNode = head;
+        int begin = 1;
+        while(begin != left){
+            beginNodeBefore = beginNode;
+            beginNode = beginNode.getNext();
+            begin ++;
+        }
+
+       // 暂存新的链   新的链的头尾节点   left 不等于right 设置头尾节点
+        Node beginListNode = new Node(beginNode.getValue());
+        Node next = beginNode.getNext();  // 前面已经判断开始和结束不相等 while必走
+        Node endListNode = new Node(next.getValue());
+        endListNode.setNext(beginListNode);
+        begin++;
+        next = next.getNext();
+        while(begin != right){
+            //依次将前一个节点设置为后一个节点的下一个节点
+            Node temp = next;
+            next = next.getNext();
+            temp.setNext(endListNode);
+            endListNode = temp;
+            begin++;
+        }
+
+        // 将尾部节点和 之前的头部节点连接
+        if(next != null){
+            beginListNode.setNext(next);
+        }
+
+        // left节点为开始节点、right节点为最开始节点    反之需要连起来
+        if(beginNodeBefore != null){
+            beginNodeBefore.setNext(endListNode);
+            return head;
+        }
+        return endListNode;
+    }
+}
+
+@Data
+class Node {
+    private Integer value;
+    private Node next;
+
+    public Node() {
+    }
+
+    public Node(Integer value) {
+        this.value = value;
+    }
+
+    public void add(Integer value) {
+        if (value == null) {
+            this.value = value;
+        } else {
+            if (next == null) {
+                next = new Node(value);
+            } else {
+                Node nextNode = next;
+                while (nextNode.next != null) {
+                    nextNode = nextNode.next;
+                }
+                nextNode.next = new Node(value);
+            }
+
+        }
+    }
+}
---
+++ src/main/java/com/zhou/demo/leetcode/ReverseStr.java
@@ -0,0 +1,82 @@
+package com.zhou.demo.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName ReverseStr
+ * @Author JackZhou
+ * @Date 2021/2/20  18:40
+ * @Desc 344. 反转字符串   https://leetcode-cn.com/problems/reverse-string/
+ **/
+public class ReverseStr {
+
+    public static void main(String[] args) {
+        //testReverseString("abcefs");
+        System.out.println(reverseStr("abcdefg", 2));
+    }
+
+    //给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。
+    //
+    //如果剩余字符少于 k 个，则将剩余字符全部反转。
+    //如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样
+    //
+    //来源：力扣（LeetCode）  541. 反转字符串 II
+    //链接：https://leetcode-cn.com/problems/reverse-string-ii
+    public static String reverseStr(String s, int k) {
+        if (s == null) {
+            return null;
+        }
+
+        char[] charArr = s.toCharArray();
+        int size = charArr.length;
+
+        int begin = 0;
+        //两种情况剩余元素大于K或者小于K
+        while (size >= (begin + k)) {  // 剩余元素大于K 、反转K
+            reverseChar(charArr, begin, begin + k - 1);
+            if ((begin + 2 * k) <= size) {
+                begin = begin + 2 * k;  // 移动2k
+            } else {
+                return new String(charArr);  //不足2k 结束
+            }
+        }
+        reverseChar(charArr, begin, size - 1); //剩余元素
+        return new String(charArr);
+    }
+
+
+    public static void reverseChar(char[] s, int begin, int end) {
+        if (s == null || s.length == 1) {
+            return;
+        }
+        while (begin < end) {
+            char temp = s[begin];
+            s[begin] = s[end];
+            s[end] = temp;
+            begin++;
+            end--;
+        }
+    }
+
+    public static void testReverseString(String str) {
+        char[] chars = str.toCharArray();
+        reverseString(chars);
+        System.out.println(Arrays.toString(chars));
+    }
+
+    public static void reverseString(char[] s) {
+        if (s == null || s.length == 1) {
+            return;
+        }
+        int begin = 0;
+        int end = s.length - 1;
+        while (begin < end) {
+            char temp = s[begin];
+            s[begin] = s[end];
+            s[end] = temp;
+            begin++;
+            end--;
+        }
+    }
+
+}
